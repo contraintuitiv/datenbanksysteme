2f61985ff0e6699d829d3cf8c9587f97459ee9fb
contraintuitiv <mail@contraintuitiv.de>
2025-10-24 16:27:38 +0200

    scanner

--- aufgabe01/sqlclient/src/CClient.java
+++ aufgabe01/sqlclient/src/CClient.java
@@ -1,6 +1,11 @@
+import java.util.Scanner;
+
 public class CClient {
     
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        Scanner reader = new Scanner(System.in);
+
+        String reader.nextLine();
     }
 }
