b581960cccd5f56c7e5220b81595db114685a571
Ruby20-50 <rubamogalli@gmail.com>
2025-10-24 17:06:49 +0200

    first commit from my side

--- aufgabe01/sqlclient/src/CClient.java
+++ aufgabe01/sqlclient/src/CClient.java
@@ -1,11 +1,30 @@
 import java.util.Scanner;
-
+import java.sql.*;
 public class CClient {
     
     public static void main(String[] args) throws Exception {
+
         System.out.println("Hello, World!");
         Scanner reader = new Scanner(System.in);
+        String query = reader.nextLine();
+        String url = "jdbc:postgresql://localhost/world?user=postgres&password=postgres";
+        Connection conn = DriverManager.getConnection(url);
 
-        String reader.nextLine();
+        Statement st = conn.createStatement();
+        ResultSet rs = st.executeQuery(query);
+        int num = rs.getMetaData().getColumnCount();
+
+
+        while (rs.next()) {
+            String result = "";
+            for (int i = 1; i <=num; i++){
+                result += rs.getString(i)+", ";
+            }
+
+            System.out.println(result);
+
+        }
+        rs.close();
+        st.close();
     }
 }
